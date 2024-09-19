dd1318990d696e268829ed919027235be7ca810a
Mariano Melchiorre <melchiorre_m@cordoba.gov.ar>
2024-09-19 11:30:59 -0300

    Correccion el errores Logos y demas se agrego archivo .env

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header className="header">
             <img 
-                src="${process.env.PUBLIC_URL}/images/tempora-logo.png" 
+                src={`${process.env.PUBLIC_URL}/images/tempora-logo.png`} 
                 alt="Tempora Logo" 
                 className="header-logo"
             />
